e26aff1b2aff0b145461e7f59e0893c9c86fe64a
Trygve <trygvebernhardt@gmail.com>
2024-03-01 15:17:56 +0100

    Oppdatering og småfiks

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Sett muggedagen til 1. juni
     const targetDate = new Date('June 1, 2024 00:00:00').getTime();
+    /* const targetDate = new Date('February 29, 2024 00:00:00').getTime(); */
+
+    console.log("Jeg er i gang")
   
     // Tell ned
     const nedtellingInterval = setInterval(function () {
@@ -10,38 +13,21 @@ document.addEventListener('DOMContentLoaded', function () {
       // Sjekk om det er den internasjonale nasjonale muggedagen
       if (timeDifference <= 0) {
         clearInterval(nedtellingInterval);
-        document.getElementById('nedtelling').innerHTML = `Gratulerer med muggedagen!`;
+        document.getElementById("nedtelling").innerHTML = `<h2 class="feiring">Gratulerer med muggedagen!</h2>`;
       } else {
         // Tell dager, timer, minutter og sekunder
         const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-  
-        // Vis nedtelling
-/*         document.getElementById('nedtelling-tekst').innerHTML = `${days} dager, ${hours} timer, ${minutes} minutter og ${seconds} sekunder igjen til muggedagen`;
- */
-        document.getElementById('nedtelling').innerHTML = `
-        <div class="enheter">
-          <div class="enhet">
-            <p class="tall">${days}</p>
-            <p class="tid">dager</p>
-          </div>
-          <div class="enhet">
-            <p class="tall">${hours}</p>
-            <p class="tid">timer</p>
-          </div>
-          <div class="enhet">
-            <p class="tall">${minutes}</p>
-            <p class="tid">timer</p>
-          </div>
-          <div class="enhet">
-            <p class="tall">${seconds}</p>
-            <p class="tid">sekunder</p>
-          </div>
-        </div>
-        <p class="undertekst">igjen til nasjonal muggedag</p>`
+        
+        // Angi tid
+        document.getElementById("days").innerText = days
+        document.getElementById("hours").innerText = hours
+        document.getElementById("minutes").innerText = minutes
+        document.getElementById("seconds").innerText = seconds
       }
-    }, 1000);
+    }, 0);
   });
   
+
